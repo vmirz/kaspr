a55972d9c4f3cbc84ea197c532e5f4e37c517937
Viktor Mirzoyan <44904186+vmirz@users.noreply.github.com>
2025-05-14 01:00:37 -0400

    More options on auto input topic creation

--- kaspr/types/models/topicsrc.py
+++ kaspr/types/models/topicsrc.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Dict
 from kaspr.types.models.base import SpecComponent
 from kaspr.types.app import KasprAppT
 from kaspr.types.topic import KasprTopicT
@@ -11,25 +11,33 @@ class TopicSrcSpec(SpecComponent):
     pattern: Optional[str]
     key_serializer: Optional[str]
     value_serializer: Optional[str]
+    partitions: Optional[int]
+    retention: Optional[int]
+    compacting: Optional[bool]
+    deleting: Optional[bool]
+    replicas: Optional[int]
+    config: Optional[Dict]
 
     app: KasprAppT = None
     _topic: KasprTopicT = None
 
     def prepare_topic(self, **kwargs) -> KasprTopicT:
         """Prepare topic based on name and pattern."""
+        options = {
+            "key_serializer": self.key_serializer,
+            "value_serializer": self.value_serializer,
+            "partitions": self.partitions,
+            "retention": self.retention,
+            "compacting": self.compacting,
+            "deleting": self.deleting,
+            "replicas": self.replicas,
+            "config": self.config,
+        }
         if self.name:
-            return self.app.topic(
-                self.name,
-                key_serializer=self.key_serializer,
-                value_serializer=self.value_serializer,
-            )
+            return self.app.topic(self.name, **options)
         else:
             # XXX pattern does not work yet
-            return self.app.topic(
-                pattern=self.pattern,
-                key_serializer=self.key_serializer,
-                value_serializer=self.value_serializer,
-            )
+            return self.app.topic(pattern=self.pattern, **options)
 
     @property
     def topic(self) -> KasprTopicT:
--- kaspr/types/schemas/topicsrc.py
+++ kaspr/types/schemas/topicsrc.py
@@ -17,7 +17,29 @@ class TopicSrcSpecSchema(BaseSchema):
     value_serializer = fields.Str(
         data_key="value_serializer", required=False, load_default=None
     )
-
+    partitions = fields.Int(
+        data_key="partitions", required=False, load_default=None, allow_none=True
+    )
+    retention = fields.Int(
+        data_key="retention", required=False, load_default=None, allow_none=True
+    )
+    compacting = fields.Bool(
+        data_key="compacting", required=False, load_default=None, allow_none=True
+    )
+    deleting = fields.Bool(
+        data_key="deleting", required=False, load_default=None, allow_none=True
+    )
+    replicas = fields.Int(
+        data_key="replicas", required=False, load_default=None, allow_none=True
+    )
+    config = fields.Mapping(
+        keys=fields.Str(required=True),
+        values=fields.Str(required=True),
+        data_key="config",
+        allow_none=True,
+        load_default=dict,
+    )
+    
     @validates_schema
     def validate_schema(self, data: Dict, **kwargs):
         if data.get("pattern") and data.get("names"):
